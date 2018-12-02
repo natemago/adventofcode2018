587338c9da01de528fce885c6d5d7af3b3bee082
Pavle Jonoski <jonoski.pavle@gmail.com>
2018-12-02 21:19:13 +0100

    :snowman: :monkey: Day 2, solved completely in Scheme.

--- day2-inventory-management/solution.scm
+++ day2-inventory-management/solution.scm
@@ -12,9 +12,6 @@
         (cond ((eof-object? line) lst)
             (else (load-input input-port (cons line lst))))))
 
-(define (sum lst)
-    (if (null? lst) 0 (+ (car lst) (sum (cdr lst)))))
-
 (define (has-count list-hash c) 
     (let ((it-does 0)) 
         (hash-table-walk list-hash (lambda (k v) 
@@ -35,3 +32,37 @@
 (display "Part 1: ")
 (display (part-1 "input"))
 (display "\n")
+
+
+(define (list-diff l1 l2 rl i) 
+    (if (null? l1) rl 
+        (if (eq? (car l1) (car l2) ) 
+            (list-diff (cdr l1) (cdr l2) rl (+ i 1) ) 
+            (list-diff (cdr l1) (cdr l2) (cons i rl) (+ i 1)) ) ))
+
+
+(define (ignore lst i n res) 
+    (if (null? lst) (reverse res) 
+        (if (= i n) 
+            (ignore (cdr lst) (+ i 1) n res) 
+            (ignore (cdr lst) (+ i 1) n (cons (car lst) res)) ) ))
+
+(define (to-string lst) 
+    (apply string lst))
+
+(define (check-all inp-list) 
+    (let ((result "<nope>")) 
+        (map (lambda (i1) 
+            (map (lambda (i2) 
+                (if (not (eq? i1 i2 )) 
+                    (let ((diff (list-diff (string->list i1) (string->list i2) '() 0 ))) 
+                        (if (= (length diff) 1) 
+                            (set! result (to-string (ignore (string->list i1) 0 (car diff) '() ) ) ) ) ) )) inp-list)) inp-list) result ) )
+
+
+(define (part-2 input-file-name) 
+    (check-all (load-input (open-input-file input-file-name) '() ) ))
+
+(display "\n\nPart 2: ")
+(display (part-2 "input"))
+(display "\n\n")
